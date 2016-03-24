f2016565cd08611379d4dc413755902c70961593
MediaLab <medialab@voyager117.evans.tamu.edu>
2016-03-23 22:34:29 -0500

    Added the littest Parse integration I've ever seen

---
+++ Rendezvous/Rendezvous/LoginViewController.swift
@@ -0,0 +1,82 @@
+//
+//  LoginViewController.swift
+//  Rendezvous
+//
+//  Created by MediaLab on 3/23/16.
+//  Copyright © 2016 GeddingtonDr. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var usernameField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func signInAction(sender: AnyObject) {
+        let username = usernameField.text ?? ""
+        let password = passwordField.text ?? ""
+        
+        PFUser.logInWithUsernameInBackground(username, password: password) { (user: PFUser?, error: NSError?) -> Void in
+            if let error = error {
+                print("User login failed.")
+                print(error.localizedDescription)
+                
+                let alert = UIAlertController(title: "User Login Failed", message: error.localizedDescription, preferredStyle: UIAlertControllerStyle.Alert)
+                alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+                self.presentViewController(alert, animated: true, completion: nil)
+            } else {
+                print("User logged in successfully")
+                self.performSegueWithIdentifier("loginSegue", sender: nil)
+            }
+        }
+    }
+    
+    @IBAction func signUpAction(sender: AnyObject) {
+        
+        let newUser = PFUser()
+        
+        // set user properties
+        newUser.username = usernameField.text
+        newUser.password = passwordField.text
+        
+        // call sign up function on the object
+        newUser.signUpInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
+            if let error = error {
+                print("User Creation failed.")
+                print(error.localizedDescription)
+                
+                let alert = UIAlertController(title: "User Creation Failed", message: error.localizedDescription, preferredStyle: UIAlertControllerStyle.Alert)
+                alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+                self.presentViewController(alert, animated: true, completion: nil)
+            } else {
+                print("User Registered successfully")
+                self.performSegueWithIdentifier("loginSegue", sender: nil)
+            }
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
