42e4d86fcf7dc47cc10bccbb7dd53c257b04db3c
BrandonArroyo <barroyo50@gmail.com>
2016-03-23 22:08:08 -0500

    added user location to the map

--- Rendezvous/Rendezvous/MapViewController.swift
+++ Rendezvous/Rendezvous/MapViewController.swift
@@ -7,12 +7,24 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController,MKMapViewDelegate,CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        self.locationManager.requestAlwaysAuthorization()
+        self.locationManager.startUpdatingLocation()
+        
+            self.mapView.showsUserLocation = true
         // Do any additional setup after loading the view.
     }
 
@@ -20,7 +32,16 @@ class MapViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.last
+        let center = CLLocationCoordinate2D(latitude: location!.coordinate.latitude, longitude: location!.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1))
+        self.mapView.setRegion(region, animated: true)
+        self.locationManager.stopUpdatingLocation()
+    }
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print("error: " + error.localizedDescription)
+    }
 
     /*
     // MARK: - Navigation
